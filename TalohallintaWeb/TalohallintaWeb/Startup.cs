85e116ba0d7e893b8b28d4c50a5eabe839831578
Minna Valtasalo <minna.valtasalo@stud.pointcollege.fi>
2017-02-25 13:52:25 +0200

    Asp.NET MVC -projektin luonti.

---
+++ TalohallintaWeb/TalohallintaWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TalohallintaWeb.Startup))]
+namespace TalohallintaWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
